151fe998023832dadc6b77f7841c0cd86ac22134
Karthik Narravula <nm6773083@gmail.com>
2026-07-16 11:02:55 +0530

    cleaned the command line runner

--- src/main/java/com/karthik/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/karthik/urlshortener/UrlShortenerApplication.java
@@ -1,24 +1,12 @@
 package com.karthik.urlshortener;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
-    @Bean
-    CommandLineRunner checkDatabase(DataSource dataSource) {
-        return args -> {
-            try (Connection conn = dataSource.getConnection()) {
-                System.out.println("Connected DB = " + conn.getCatalog());
-                System.out.println("URL = " + conn.getMetaData().getURL());
-            }
-        };
-    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerApplication.class, args);
